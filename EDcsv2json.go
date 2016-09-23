451c6dfb576c9806567ed9daecaa43bd8c166a90
Roy Dybing <roy.dybing@gmail.com>
2016-09-23 20:09:53 +0200

    Working as intended - Input and output files included

--- EDcsv2json.go
+++ EDcsv2json.go
@@ -1,3 +1,14 @@
+/* small piece of code to convert a rather lenghty (132 lines) csv list
+ * of Elite: Dangerous materials to a more managable json format.
+ *
+ * Original file is *NOT* comma separated, but tab separated...
+ *
+ * blatantly copied from:
+ * https://www.socketloop.com/tutorials/golang-convert-csv-data-to-json-format-and-save-to-file
+ * with some minor changes to make it work for my purposes.
+ * CC-BY Roy Dybing September 2016
+ */
+
 package main
 
 import (
@@ -16,19 +27,16 @@ type Material struct {
 
 func main() {
 	// read data from CSV file
-
 	csvFile, err := os.Open("./materials.csv")
 
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	defer csvFile.Close()
 
 	reader := csv.NewReader(csvFile)
-
+	reader.Comma = '\t' // Use tab-delimited instead of comma
 	reader.FieldsPerRecord = -1
-
 	csvData, err := reader.ReadAll()
 
 	if err != nil {
@@ -53,13 +61,9 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	// sanity check
-	// NOTE : You can stream the JSON data to http service as well instead of saving to file
+	// Terminal output (optional, debug only)
 	fmt.Println(string(jsondata))
-
-	// now write to JSON file
-
+	// JSON file output
 	jsonFile, err := os.Create("./materials.json")
 
 	if err != nil {
